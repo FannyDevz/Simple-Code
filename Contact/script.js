4ca6e274d00379a21aecb17b1d7b27048a08bf5e
Fanny <fannybagus9f@gmail.com>
2024-02-06 09:28:00 +0700

    Initial commit

---
+++ Contact/script.js
@@ -0,0 +1,9 @@
+function showPopup() {
+    var popup = document.getElementById("popupContent");
+    popup.style.display = "block";
+}
+
+function hidePopup() {
+    var popup = document.getElementById("popupContent");
+    popup.style.display = "none";
+}
